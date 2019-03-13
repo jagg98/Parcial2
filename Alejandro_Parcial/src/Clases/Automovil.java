f927db7b69c4c85096b2a230f9ae76e284b7ac33
jagg98 <alejandrogarciagomeztst@gmail.com>
2019-03-13 08:37:47 -0500

    A master

---
+++ Alejandro_Parcial/src/Clases/Automovil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alejandro_parcial.Clases;
+
+/**
+ *
+ * @author USER
+ */
+public class Automovil extends Vehiculo{
+    
+    public Automovil(String placa, String marca, float valorPeaje) {
+        super(placa, marca, 5000, "Automovil");
+    }
+    
+}
